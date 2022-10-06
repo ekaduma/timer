93e58ce26a955daa334d49412fb895f4db072483
ekaduma <eka.dumaita@gmail.com>
2022-10-06 20:07:50 +0700

    timer in react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Timer from "./Timer";
 
 function App() {
+  const [text, setText] = useState([]);
+  const [input, setInput] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (input === "") {
+      return alert("please input a text");
+    }
+
+    const newText = {
+      id: Date.now(),
+      title: input.text,
+      timer: 100,
+    };
+
+    setText((prevText) => {
+      return [...prevText, newText];
+    });
+    setInput("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="p-2 space-x-2 mt-20 flex justify-center ">
+        <input
+          className="p-2 rounded-md w-80 text-white bg-blue-400 shadow-md placeholder-white font-semibold"
+          placeholder="Enter Name"
+          value={input ? input.text : input}
+          onChange={(e) => {
+            setInput({ ...input, text: e.target.value });
+          }}
+        ></input>
+        <button className="bg-green-300 p-2 rounded-md shadow-md text-white font-semibold" onClick={onSubmit}>
+          Add
+        </button>
+      </form>
+
+      <div className="mt-10 flex justify-center">
+        <table className="w-96 table-fixed">
+          <thead>
+            <tr className="border-2 bg-blue-300  text-white">
+              <th className="p-2">Nama</th>
+              <th>Waktu</th>
+            </tr>
+          </thead>
+          {text.map((i) => (
+            <Timer key={i.id} data={i} />
+          ))}
+        </table>
+      </div>
     </div>
   );
 }
---
+++ src/Timer.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+const Timer = ({ data }) => {
+  const [timer, setTimer] = useState(data.timer);
+  const [done, setDone] = useState(false);
+
+  useEffect(() => {
+    const x = setInterval(() => setTimer((timer) => timer - 1), 1000);
+    if (timer === 0) {
+      setDone(true);
+    }
+    return () => {
+      clearInterval(x);
+    };
+  }, [timer]);
+
+  return (
+    <tbody>
+      <tr className="bg-green-300 text-center border-2 text-white">
+        <td className="p-1">{data.title}</td>
+        <td>{done ? "Waktu Habis" : timer}</td>
+      </tr>
+    </tbody>
+  );
+};
+
+export default Timer;
